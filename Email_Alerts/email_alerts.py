89e70f1a35ca69b905dae6795223a2d148e41337
Robert Hayden Davis <HD7017@gmail.com>
2025-02-08 16:39:38 -0600

    Initial Commit

---
+++ Email_Alerts/email_alerts.py
@@ -0,0 +1,31 @@
+import smtplib
+import pandas as pd
+import datetime
+import socket
+
+
+# Email Configuration
+
+
+# Read CSV Data
+
+
+# Set Threshold Values
+
+
+# Check for Warranty Expirations
+
+
+# Check for Software License Expirations
+
+
+# Check for Offline Devices
+
+
+# Check Live Network Status
+
+
+# Construct Email Alert Message
+
+
+# Send Email if Alerts Exist
